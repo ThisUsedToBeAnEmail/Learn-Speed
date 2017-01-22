4e9600b49a67538b29dedfcf7f7252a789dafe14
LNATION <thisusedtobeanemail@gmail.com>
2017-01-22 11:18:36 +0100

    array

---
+++ basic_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int numbers[5];
+	
+    /* populate the array .... */
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+    
+    printf("The 5th number in the array is %d", numbers[4]);
+	return 0;
+}
